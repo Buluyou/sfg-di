bf3395d04394acf3601a5cec12f3704fd6a1daa1
XIAO <jinxiao.shawn@gmail.com>
2021-03-23 15:04:54 +0800

    Add primary and profile in java config registrition(primary-profile)

--- src/main/java/guru/springframework/sfgdi/services/I18nChineseGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nChineseGreetingService.java
@@ -1,10 +1,5 @@
 package guru.springframework.sfgdi.services;
 
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
-
-@Profile({"CN", "default"})
-@Service("i18nService")
 public class I18nChineseGreetingService implements GreetingService{
     @Override
     public String sayGreeting() {
